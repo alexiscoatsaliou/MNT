2f1d82e37b1c7e067417d90f9d96fdfe28ef0685
LePaulM <p.miancien@laposte.net>
2017-03-03 16:06:13 +0100

    MNT finis II

--- MNT.java
+++ MNT.java
@@ -2,6 +2,8 @@ package ProjetJava;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class MNT {
@@ -19,19 +21,19 @@ public class MNT {
 		tab = new Point [nline][ncols];
 	}
 
-		public MNT LireMNT(){
+		public MNT LireMNT() throws Exception{
 			MNT mnt = null;
 			
-			String filename = "D:\\mnt_test.txt";
+			String filename = "D:\\Dpt_75_asc.asc";
 			File f = new File(filename);
 			Scanner in = null;
-			String ligne;
 			int ncols = 0;
 			int nrows = 0;
 			int pas = 0;
 			int compteur = 1;
 			double coordX = 0;
-			double coordY = 0;		
+			double coordY = 0;
+			DecimalFormat format = new DecimalFormat("#.00");
 			
 			try {
 			in = new Scanner(f);
@@ -40,17 +42,17 @@ public class MNT {
 				if (line.contains("ncols "))
 				{
 					String[] st = line.split("ncols ", ' ');
-					ncols = Integer.parseInt(st[1]);
+					ncols = format.parse(st[1]).intValue();
 				}
 				if (line.contains("nrows "))
 				{
-					String[] st = line.split("nrows", ' ');
-					nrows = Integer.parseInt(st[1]);
+					String[] st = line.split("nrows ", ' ');
+					nrows = format.parse(st[1]).intValue();
 				}
 				if (line.contains("cellsize "))
 				{
 					String[] st = line.split("cellsize ", ' ');
-					pas = Integer.parseInt(st[1]);
+					pas = format.parse(st[1]).intValue();
 				}
 				if (line.contains("xllcorner "))
 				{
@@ -65,22 +67,23 @@ public class MNT {
 				mnt = new MNT(ncols, nrows, pas);
 				compteur++;
 				
-				if (compteur >= 7){
+				if (compteur >= 8){
 					
 					for (int i = 0; i < ncols; i++) {
 						String[] st = line.split(" ");
-						int j = 1;
-						while ( j != ncols) {
+						int j = 0;
+						while ( j != nrows) {
 							double z = Double.parseDouble(st[j]);
 							
 							tab[i][j] = new Point(coordX + ((double)pas * (double)i), coordY + ((double)pas * (double)j), z, i, j);
 							
-							System.out.println(tab);
+							System.out.print (tab[i][j]);
 							
+							j++;
 						}
 						st = null;
 					}
-				} 
+				}
 			}
 			
 			
@@ -94,7 +97,7 @@ public class MNT {
 			return mnt;
 
 		}
-
+		
 }
 
 
