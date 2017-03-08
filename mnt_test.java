d0a66ae185a5172c00f6450f18e39f45ddca4739
alexiscoatsaliou <alexis.coatsaliou@ensg.eu>
2017-03-08 11:22:53 +0100

    Update mnt_test.java

--- mnt_test.java
+++ mnt_test.java
@@ -14,5 +14,17 @@ public class mnt_test {
 	    LireMNT();
 
 	  }
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setSize(new Dimension(1000,1000));
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		TestJava2D panelJava2D = new TestJava2D();
+		panelJava2D.setPreferredSize(new Dimension(1000,1000));
+		frame.setContentPane(panelJava2D);
+		frame.setVisible(true);
+	}
 	
 }
