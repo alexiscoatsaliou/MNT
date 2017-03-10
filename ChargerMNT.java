5e2ea2b88d6402e288ef533f2398021a212219ac
alexiscoatsaliou <alexis.coatsaliou@ensg.eu>
2017-03-10 09:28:39 +0100

    Update ChargerMNT.java

--- ChargerMNT.java
+++ ChargerMNT.java
@@ -4,26 +4,47 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.JTextField;
+import javax.swing.JLabel;
 
+public class ChargerMNT extends JButton implements ActionListener {
 
-public class ChargerMNT implements ActionListener {
-
-	private JTextField _textField;
-	String chemin;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
-	public void actionPerformed(ActionEvent e) { 
+	
+	static String chemin;
+	private JLabel label;
+	static Point[][] tab;
+	static MNT m;
+	
+	public ChargerMNT(String msg, JLabel label) {
+		super(msg);
+		this.label = label;
+		this.addActionListener(this);
+		
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) { 		// Ouvre une fenêtre pour séléctionner le MNT
 	
 		 JFileChooser chooser = new JFileChooser();
 		    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);   
 		    int returnVal = chooser.showOpenDialog(null);
 		    if(returnVal == JFileChooser.APPROVE_OPTION) 
 		    {
-		    	 File folder = chooser.getSelectedFile();
-		         chemin = folder.getPath();
-		    }
-		    
+		    	File folder = chooser.getSelectedFile();
+		        chemin = folder.getPath();
+		        this.label.setText("Fichier MNT sélectionné : " + chemin);
+		        
+		    	ChargerMNT.m = new MNT(ChargerMNT.chemin);
+		        ChargerMNT.tab = ChargerMNT.m.LireMNT();
+		        
+		        AffichageMNT affiche = new AffichageMNT(); 		//Afficher le MNT
+		    } 
 	}
-	
+
 }
